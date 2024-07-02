e566f403f255e2ca438320250fcbcd0548e0898a
ashmit1795 <ashmitpatra77@gmail.com>
2024-07-02 19:12:37 +0530

    minor debugs

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,4 @@
-const asyncHandler = (func) => {
+export const asyncHandler = (func) => {
     return (req, res, next) => {
         Promise.resolve(func(req, res, next)).catch((err) => next(err))
     }
