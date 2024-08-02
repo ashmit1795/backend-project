d7df9c30d29e89ac53f97f3f3bf57a5da54ee4e3
ashmit1795 <ashmitpatra77@gmail.com>
2024-08-02 15:25:30 +0530

    add full-text search index for video titles and descriptions

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -34,8 +34,12 @@ const videoSchema = new Schema({
         type: Boolean,
         default: true
     },
+    tags: [{
+        type: String
+    }]
 }, {timestamps: true});
 
+videoSchema.index({ title: "text", description: "text" });
 videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
